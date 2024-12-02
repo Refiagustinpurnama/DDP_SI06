2583a27fbe0b5e880035df9d8fdb39090be2e27e
refiagustinpp <repiagustin20@gmail.com>
2024-12-02 21:31:59 +0700

    Ddp3.py

--- ddp3.py
+++ ddp3.py
@@ -38,3 +38,54 @@ print('berat badan ideal adalah :',bb_ideal)
 
 # 4. Buat program python untuk mencari nilai konversi dari celcius ke fahreinthel
 # rumus + (celcius *9/5) + 32s
+# Menghitung Celcius Ke Fahrenheit
+print()
+print('===Suhu Celcius Ke Fahrenheit===')
+
+# Input Perhitungan
+celcius = int(input('Masukan Suhu Celcius: '))
+
+# Proses Perhitungan
+rumus_fahrenheit = int(celcius*9/5)+32
+
+# Output Perhitungan
+print(celcius, 'Celcius sama dengan', rumus_fahrenheit, 'Fahrenheit')
+
+
+
+
+# Menghitung Luas Tabung
+print("")
+print("=====Luas Tabung======")
+
+# Input Angka
+phi = 3.14
+jari2 = int(input('Masukan nilai jari-jari: '))
+tinggi = int(input('Masukan nilai tinggi: '))
+
+# Proses Perhitungan
+luas_permukaan =  2*phi*jari2*(jari2+tinggi)
+luas_alas = phi*jari2**2
+luas_selimut = 2 * phi * jari2 * tinggi
+
+# Output Perhitungan
+print('Luas Permukaan Tabung Adalah: ', luas_permukaan)
+print('Luas Alas Tabung Adalah: ', luas_alas)
+print('Luas Selimut Tabung Adalah: ', luas_selimut)
+
+
+
+# Menghitung Keliling Tabung
+print()
+print('===Keliling Tabung===')
+
+# Input Angka
+r = int(input("Masukan Jari-Jari: "))
+tinggi = int(input("Masukan tinggi: "))
+phi = 3.14
+
+# Proses Perhitungan
+rumus = 2*phi*r*tinggi
+
+# Output Perhitungan
+print('Keliling Tabung Adalah', rumus)
